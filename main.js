a46afadee38c97c9e19584b6441b416ce0392cc1
ibrahimdev23 <ibrahim.13mohamed@gmail.com>
2022-03-02 18:18:37 -0600

    finishing the lab

--- main.js
+++ main.js
@@ -8,18 +8,24 @@ let getBtn = document.querySelector('.btn')
 getBtn.addEventListener("click", () => {
     
     axios.get('https://swapi.dev/api/planets/?search=Alderaan')
-    .then(res => {
-        for (let i = 0; i < res.data.residents.length; i++) {
-            axios(`${res.data.residents[i]}`)
-            .then(res => {
-                const h2 = document.createElement('h2');
-                h2.textContent = res.data.name;
-                document.querySelector('body').appendChild(h2);
-            });
-        };
-    });
-})
+         .then(res => {
+            let residentsArr = res.data.results[0].residents
+            for(let i=0; i < residentsArr.length; i++){
+                axios.get(residentsArr[i])
+                     .then(res => {
+                         let h2 = document.createElement('h2')
+                         h2.textContent = res.data.name
+                         document.querySelector('body').appendChild(h2)
+                     })
+            }
+        })
 
-button.addEventListener("click", buttonFunc);
 
-    
+
+
+
+
+
+
+
+    })
